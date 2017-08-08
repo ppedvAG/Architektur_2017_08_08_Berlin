cc04ddc836993056647546836336ad370f73a08c
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-08-08 09:36:59 +0200

    Add project files.

---
+++ SingeltonExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SingeltonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
